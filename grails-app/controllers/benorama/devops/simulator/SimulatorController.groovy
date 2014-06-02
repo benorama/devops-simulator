68f9613e7a85976bd4dcd740195b111d06e886c1
Benoit Hediard <ben@agorapulse.com>
2014-06-02 15:39:09 +0200

    Minor update to pass id or throw an error for demo purpose

--- grails-app/controllers/benorama/devops/simulator/SimulatorController.groovy
+++ grails-app/controllers/benorama/devops/simulator/SimulatorController.groovy
@@ -14,12 +14,15 @@ class SimulatorController {
         if (params.pipeline && Pipeline.contains(params.pipeline)) {
             session.pipeline = params.pipeline.toUpperCase() as Pipeline
         }
+        if (params.error) {
+            throw new Exception("Some error... throw an exception to demo Sentry exception monitoring!")
+        }
         Pipeline pipeline = session.pipeline
         Account account = Account.get(session.accountId)
         [
                 account: account,
-                deployment: params.deploy ? simulatorService.deployApp(account, pipeline) : [:],
-                reply: params.message ? simulatorService.sendMessage(account, pipeline) : [:],
+                deployment: params.deploy ? simulatorService.deployApp(account, pipeline, params.int("deploy")) : [:],
+                reply: params.message ? simulatorService.sendMessage(account, pipeline, params.int("reply")) : [:],
                 pipeline: pipeline
         ]
     }
